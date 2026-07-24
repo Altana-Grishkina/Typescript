0a8b8750304bb3e6a4ad3cebb07c9965ad31ea01
Altana <altn.grishkina@capgemini.com>
2026-07-24 11:51:31 +0200

    setup new chapter & 59. var & let & const differences

---
+++ 5_next-gen-01-starting-setup/src/app.ts
@@ -0,0 +1,21 @@
+const userName = 'Max';
+
+let age = 30;
+age = 31;
+
+
+// we can access to result outside of scope in JavaScript with var
+/* function add(a: number, b: number){
+    var result = a + b;
+    return result;
+}
+
+console.log(result); */
+
+// we can't access to result outside of scope in JavaScript with let
+/* function add(a: number, b: number){
+    let result = a + b;
+    return result;
+}
+
+console.log(result); */
