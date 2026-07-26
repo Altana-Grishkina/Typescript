5dd0666eacb670e678f8977d5129f734c34e9f27
Altana <altn.grishkina@capgemini.com>
2026-07-26 15:01:21 +0200

    60. Arrow functions

--- 5_next-gen-01-starting-setup/src/app.ts
+++ 5_next-gen-01-starting-setup/src/app.ts
@@ -19,3 +19,19 @@ console.log(result); */
 }
 
 console.log(result); */
+
+
+
+// 60. Arrow functions
+
+const add = (a: number, b: number) => a + b;
+
+const printOutput:(a: number | string) => void = output => console.log(output);
+
+const button = document.querySelector('button');
+if(button){
+    button.addEventListener('click', event => console.log(event));
+}
+button
+
+printOutput(add(2, 5));
