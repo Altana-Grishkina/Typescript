040c4ceb58d4e3699ea3762f874120ab2f2338b2
Altana <altn.grishkina@capgemini.com>
2026-08-24 01:21:11 +0200

    72. Marking Fields as readonly

--- 6_classes_interfaces/basics.ts
+++ 6_classes_interfaces/basics.ts
@@ -1,7 +1,7 @@
 // Shortcut for defining a class with a constructor that automatically assigns parameters to public properties.
 class User {
-    private hobbies: string[] = [];
-    constructor(public name: string, protected age: number) {
+    public readonly hobbies: string[] = [];
+    constructor(public name: string, private readonly age: number) {
        
     }
 
@@ -10,7 +10,15 @@ class User {
     }
 }
 
+
 const max = new User("Max", 36);
 const fred = new User("Fred", 40);
 
 console.log(max, fred);
+
+// Doesn't work
+// max.hobbies = ['Sports'];
+
+// Work with push
+max.hobbies.push('Sports');
+
